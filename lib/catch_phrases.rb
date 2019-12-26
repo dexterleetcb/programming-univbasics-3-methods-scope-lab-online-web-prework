a85f2c0e250a4490e78cd0abb2a90240d198ac06
Dexter Roussel <dexterleetcb@gmail.com>
2019-12-26 19:44:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,3 +11,16 @@ describe "toadstool" do
     expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
   end
 end
+
+describe "link" do
+  it "puts out 'It's Dangerous To Go Alone! Take This.'"do
+    expect{link}.to ouput("It's Dangerous To Go Alone! Take This.\n").to_stdout
+    end
+  end
+  
+  describe "any_phrase" do
+    it "takes in an argument and puts out the catch phrase" do
+      phrase = "Do A Barrel Roll!"
+      expect{any_phrase(phrase)}.to output("Do A Abarrel Roll!\n".to_stdout
+    end
+  end
