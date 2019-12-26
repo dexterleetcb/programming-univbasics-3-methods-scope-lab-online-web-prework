7c6d2f82299713d6df7dbc4f178b16e001ec860e
Dexter Roussel <dexterleetcb@gmail.com>
2019-12-26 19:38:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,3 +5,9 @@ describe "mario" do
     expect{mario}.to output("It's--a me, Mario!\n").to_stdout
   end
 end
+
+describe "toadstool" do
+  it "puts out 'Thank you Mario! But Our Princess Is In Another Castle!'" do
+    expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
+  end
+end
